d48e9cc2329533cba7af1b0d4b443dd747d16cda
Daniel Murphy <danielmurphy50@gmail.com>
2019-02-14 19:55:24 +0000

    Add files via upload

---
+++ UI/EnemyUi.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class EnemyUi : MonoBehaviour {
+
+    public Text North;
+    public Text East;
+    public Text West;
+    public Text South;
+
+
+    public void UpdateUi(List<EnemyAttackSide> AtkSideList) {
+        foreach (EnemyAttackSide atkside in AtkSideList) {
+            switch (atkside.GetDirection())
+            {
+                case Enums.Direction.East:
+                    East.text = atkside.GetEnemyCount().ToString();
+                    break;
+                case Enums.Direction.West:
+                    West.text = atkside.GetEnemyCount().ToString();
+                    break;
+                case Enums.Direction.North:
+                    North.text = atkside.GetEnemyCount().ToString();
+                    break;
+                case Enums.Direction.South:
+                    South.text = atkside.GetEnemyCount().ToString();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ UI/PowerUi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine.UI;
+using UnityEngine;
+namespace Assets.UI
+{
+    class PowerUi: MonoBehaviour
+    {
+      
+        Text PowerText;
+
+        public void Start() {
+            PowerText = GameObject.Find("PowerUIValue").GetComponent<Text>();
+        }
+
+
+        public void UpdateUi(float pow) {
+            float PowerLevel = pow;
+            PowerText.text = pow.ToString();
+            Debug.Log(pow.ToString());
+
+            if (PowerLevel > 90)
+            {
+                PowerText.color = Color.green;
+            }
+            else if (PowerLevel > 50)
+            {
+                PowerText.color = Color.yellow;
+            }
+            else
+            {
+                PowerText.color = Color.red;
+            }
+        }
+
+
+    }
+}
